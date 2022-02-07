f9dcf7443e994b7f16b6f6dada335d621d251396
elayudante <juanma@elayudante.es>
2022-02-07 13:57:32 +0100

    añadidas algunas tareas y creado el componente task, además de añadir lla librearía react-icons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import Header from "./components/Header";
+import { useState } from 'react'
+import Header from "./components/Header"
+import Tasks from "./components/Tasks"
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Cita con el médico',
+      day: '5 de febrero 14:30',
+      reminder: true
+    },
+    {
+      id: 2,
+      text: 'Tutoría en el colegio',
+      day: '6 de febrero 13:30',
+      reminder: true
+    },
+    {
+      id: 3,
+      text: 'Salir de la cuarentena',
+      day: '6 de febrero 12:30',
+      reminder: true
+    }
+  ])
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
