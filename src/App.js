d49b9f003fe6d4c062761f66f7f0f1370c2eefe5
elayudante <juanma@elayudante.es>
2022-02-07 14:32:13 +0100

    añadida funcion para eliminar tarea y para recordar

--- src/App.js
+++ src/App.js
@@ -23,10 +23,25 @@ const App = () => {
       reminder: true
     }
   ])
+
+  // Eliminar tarea
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  // Recordatorio
+  const toggleReminder = (id) => {
+    console.log(id)
+  }
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks}/>
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
+      ) : (
+        'Sin tareas que mostrar'
+      )}
     </div>
   );
 }
